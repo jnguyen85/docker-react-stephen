0ef5d6ab680039f835f47841ffe30bfb9bc7003c
John Nguyen <nguyen8585@gmail.com>
2021-11-22 09:22:38 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there! not changing environment:
+          - CHOKIDAR_USEPOLLING=true
         </p>
         <a
           className="App-link"
