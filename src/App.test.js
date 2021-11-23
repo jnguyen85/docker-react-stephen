00c9ae14919ec32d4c5aebc66c68dcc76179c4ae
John Nguyen <nguyen8585@gmail.com>
2021-11-22 18:59:26 -0800

    fix nginx Dockerfile

--- src/App.test.js
+++ src/App.test.js
@@ -8,18 +8,3 @@ test('renders learn react link', () => {
 });
 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-
-
